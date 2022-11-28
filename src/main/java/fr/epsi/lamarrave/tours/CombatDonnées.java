0010b47fdab4af3b3c4b18e47d03f3dcb1a84e9b
Tristan Muller <t.muller@ipconect.fr>
2022-11-28 12:37:32 +0100

    feat: :sparkles: Implémenter les données du combat

--- src/main/java/fr/epsi/lamarrave/tours/CombatDonnées.java
+++ src/main/java/fr/epsi/lamarrave/tours/CombatDonnées.java
@@ -2,17 +2,12 @@ package fr.epsi.lamarrave.tours;
 
 import fr.epsi.lamarrave.personnages.Adversaire;
 import fr.epsi.lamarrave.personnages.Personnage;
+import fr.epsi.lamarrave.personnages.PersonnageFabrique;
 
 /**
  * Stock les informations d'un combat pour les classes "TourCombat" et "AttaqueCommande"
  */
 public class CombatDonnées {
-
-	/**
-	 * Default constructor
-	 */
-	public CombatDonnées() {
-	}
 
 	/**
 	 * Adversaire opposant le héro pendant un combat (si non-défini, alors doit être tiré aléatoirement)
