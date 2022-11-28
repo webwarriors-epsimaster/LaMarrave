154b521425acc2f80df20191ac61f1f95bfa67a8
development <development@debian-BULLSEYE-live-builder-AMD64>
2022-11-28 22:19:08 +0100

    Ajout de la commande de montée de niveau au sein d'un tour de maitre d'arme

--- src/main/java/fr/epsi/lamarrave/personnages/SylvieMalezieu.java
+++ src/main/java/fr/epsi/lamarrave/personnages/SylvieMalezieu.java
@@ -11,12 +11,33 @@ public class SylvieMalezieu extends Adversaire {
 	 * Default constructor
 	 */
 	public SylvieMalezieu() {
+		this.force += 5;
+		this.vie += 5;
+		this.armure += 2;
 	}
 
 	@Override
 	public String récupérerAttaque() {
-		// TODO Auto-generated method stub
-		return null;
+		double taChance = Math.random()*10;
+		if (taChance <7) {
+			return sortCommun();
+		}
+		else if(taChance<9) {
+			return sortCritique();
+		}
+		else return sortUltime();
+	}
+	
+	public String sortCommun(){
+		return "attaque commune vous n'aurez pas très mal";
+	}
+	public String sortCritique() {
+		this.force += 10;
+		return "attaque critique, tu n'as pas de chance";
+	}
+	public String sortUltime() {
+		this.force += 15;
+		return "attaque ultime, si tu survis pense à remercier les dieux";
 	}
 
 }
